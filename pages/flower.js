b5aa3afc9ad80cf972a7b8e1db342e080cbe690a
Nirajan Acharya <nirajanach@gmail.com>
2023-10-11 12:21:42 +1000

    Added animation to play around

---
+++ pages/flower.js
@@ -0,0 +1,15 @@
+import Flower from "../components/flower";
+import Layout from "../components/layout";
+
+
+
+export default function FlowerPage() {
+  return (
+    <Layout>
+      <div>
+        {/* Render the Flower component here */}
+        <Flower />
+      </div>
+    </Layout>
+  );
+}
